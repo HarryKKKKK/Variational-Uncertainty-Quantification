64a309601826fbb3109c4fdbf5817366723417ee
Harry Kong <hk1122@ic.ac.uk>
2025-06-19 07:34:32 +0100

    UPDATE: main - largely adjust the layout to reduce spaces and center the classes.

--- static/js/index.js
+++ static/js/index.js
@@ -13,7 +13,31 @@ $(document).ready(function () {
   bulmaCarousel.attach('.carousel', options);
   bulmaSlider.attach();
 
-  // === Plotly data & layout ===
+  // === Plotly axis and heatmap preparation ===
+    const xmin = -4;
+    const xmax = 4;
+    const ymin = -4;
+    const ymax = 4;
+    const step = 0.5;
+
+    const x = [];
+    const y = [];
+    for (let val = xmin; val <= xmax; val += step) {
+      x.push(val);
+    }
+    for (let val = ymin; val <= ymax; val += step) {
+      y.push(val);
+    }
+
+    const z = [];
+    for (let i = 0; i < y.length; i++) {
+      const row = [];
+      for (let j = 0; j < x.length; j++) {
+        row.push(0); 
+      }
+      z.push(row);
+    }
+
   const trace1 = {
     x: [],
     y: [],
@@ -23,28 +47,68 @@ $(document).ready(function () {
     hoverinfo: 'none'
   };
 
+  const customColorscale = [
+    [0, 'white'],
+    [0.2, 'rgba(220,230,247,1)'], 
+    [0.4, 'rgba(189,215,231,1)'],
+    [0.6, 'rgba(107,174,214,1)'],
+    [0.8, 'rgba(49,130,189,1)'],
+    [1, 'rgba(8,81,156,1)']
+  ];
+
   const heatmap = {
-    z: [
-      [0.1, 0.2, 0.3, 0.4, 0.5],
-      [0.2, 0.3, 0.4, 0.5, 0.6],
-      [0.3, 0.4, 0.5, 0.6, 0.7],
-      [0.4, 0.5, 0.6, 0.7, 0.8],
-      [0.5, 0.6, 0.7, 0.8, 0.9]
-    ],
-    x: [-2, -1, 0, 1, 2],
-    y: [-2, -1, 0, 1, 2],
+    z: z,
+    x: x,
+    y: y,
     type: 'heatmap',
-    colorscale: 'Blues',
+    colorscale: customColorscale,
+    zmin: 0, 
+    zmax: 1,
     opacity: 0.6,
     hoverinfo: 'none'
   };
 
-  const layout = {
-    margin: { t: 0 },
-    xaxis: { range: [-4, 4], fixedrange: true },
-    yaxis: { range: [-4, 4], fixedrange: true }
-  };
+  // === Plotly axis and layout setup ===
+  const allTickValsX = [];
+  const allTickTextX = [];
+  const allTickValsY = [];
+  const allTickTextY = [];
 
+  for (let val = xmin; val <= xmax; val += step) {
+      allTickValsX.push(val);
+      allTickTextX.push(Number.isInteger(val) ? val.toString() : '');
+  }
+  for (let val = ymin; val <= ymax; val += step) {
+      allTickValsY.push(val);
+      allTickTextY.push(Number.isInteger(val) ? val.toString() : '');
+  }
+
+  const layout = {
+      height: 600,
+      margin: {t:10},
+      xaxis: {
+          range: [xmin, xmax],
+          fixedrange: true,
+          tickvals: allTickValsX,
+          ticktext: allTickTextX,
+          gridcolor: 'rgba(200, 200, 200, 0.8)', 
+          tickcolor: 'rgba(200, 200, 200, 0.8)', 
+          ticks: 'outside',    
+          ticklen: 8, 
+          tickwidth: 1.5   
+      },
+      yaxis: {
+          range: [ymin, ymax],
+          fixedrange: true,
+          tickvals: allTickValsY,
+          ticktext: allTickTextY,
+          gridcolor: 'rgba(200, 200, 200, 0.8)', 
+          tickcolor: 'rgba(200, 200, 200, 0.8)',     
+          ticks: 'outside',         
+          ticklen: 8,
+          tickwidth: 1.5   
+      }
+  };
   const config = { displayModeBar: false };
 
   // === Helper Functions ===
@@ -104,42 +168,6 @@ $(document).ready(function () {
     infoDiv.innerHTML = infoHTML;
   });
 
-  // === Init plot + Click add points ===
-  Plotly.newPlot('plot', [heatmap, trace1], layout, config).then(function (gd) {
-    const xaxis = gd._fullLayout.xaxis;
-    const yaxis = gd._fullLayout.yaxis;
-
-    gd.on('plotly_click', function (data) {
-      const x = roundToHalf(data.points[0].x);
-      const y = roundToHalf(data.points[0].y);
-
-      if (isPointExists(x, y)) {
-        alert('This dot has been added already');
-        return;
-      }
-
-      const color = prompt('Enter color: red or blue');
-      if (!color || !['red', 'blue'].includes(color.toLowerCase())) return;
-
-      const newX = trace1.x.concat(x);
-      const newY = trace1.y.concat(y);
-      const newColor = trace1.marker.color.concat(color);
-
-      trace1.x = newX;
-      trace1.y = newY;
-      trace1.marker.color = newColor;
-
-      Plotly.react('plot', [heatmap, trace1], layout, config).then(function (gdNew) {
-        gdNew.on('plotly_click', arguments.callee);
-      });
-
-      const infoDiv = document.getElementById('point-info');
-      let infoHTML = '<strong>Added dots:</strong><ul>';
-      for (let i = 0; i < trace1.x.length; i++) {
-        infoHTML += `<li>Point ${i + 1}: (${trace1.x[i].toFixed(2)}, ${trace1.y[i].toFixed(2)}), ${trace1.marker.color[i]}</li>`;
-      }
-      infoHTML += '</ul>';
-      infoDiv.innerHTML = infoHTML;
-    });
-  });
+  // === Init plot  ===
+  Plotly.newPlot('plot', [heatmap, trace1], layout, config);
 });
